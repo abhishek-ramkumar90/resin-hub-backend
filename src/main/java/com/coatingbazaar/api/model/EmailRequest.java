911c37209c6b6b45f78432ad746180a900d834d7
Abhishek ramkumar <abhishek90.iyer@gmail.com>
2026-06-08 11:55:19 +0200

    initial commit

---
+++ src/main/java/com/coatingbazaar/api/model/EmailRequest.java
@@ -0,0 +1,18 @@
+package com.coatingbazaar.api.model;
+
+public record EmailRequest(
+	String subject,
+	String quantity,
+	String companyname,
+	String pincode,
+	String contactnumber,
+	String category,
+	String product,
+	String industry,
+	String colour,
+	String chemistry,
+	String finish,
+	String gloss
+) {
+}
+
